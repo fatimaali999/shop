2d36d66e168464ef6e055751e0ad7e4cc96c9b34
Nanakzy <nanakzy@gmail.com>
2024-09-25 18:13:56 +0000

    created migration link -app&db

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,19 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
 jwt = JWTManager()
+migrate = Migrate()  # Initialize Flask-Migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Initialize the extensions
     db.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)  # Link Flask-Migrate with app and database
     CORS(app)  # Enable Cross-Origin Resource Sharing
 
+    # Import and register your blueprints
     from app.routes import main_routes
     app.register_blueprint(main_routes)
 
