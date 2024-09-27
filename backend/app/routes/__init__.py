7c7e77da5b1b9d8c90573b2ee3ab90e98ab8f341
Nomee07 <gigabanomfundo@gmail.com>
2024-09-27 14:28:00 +0200

    Delete backend

--- backend/app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from .main_routes import main_routes
-from .user_routes import user_routes
-
-app_routes = Blueprint('app_routes', __name__)
-
-app_routes.register_blueprint(main_routes)
-app_routes.register_blueprint(user_routes)
